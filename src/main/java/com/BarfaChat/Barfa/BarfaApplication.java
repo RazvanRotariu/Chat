c57204bb633de836cac346a182a964535cc0b01d
Razvan <rotariurazvan2@gmail.com>
2022-09-23 13:54:27 +0100

    Add Springboot template!

---
+++ src/main/java/com/BarfaChat/Barfa/BarfaApplication.java
@@ -0,0 +1,13 @@
+package com.BarfaChat.Barfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarfaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarfaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BarfaChat/Barfa/BarfaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BarfaChat.Barfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
