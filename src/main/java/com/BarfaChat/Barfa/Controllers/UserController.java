0b9373307f2e7f2a1167b8b101e7c3cf69d38961
AvrIoan <opsidoxro@gmail.com>
2022-09-24 22:04:25 +0300

    Update UserController.java

--- src/main/java/com/BarfaChat/Barfa/Controllers/UserController.java
+++ src/main/java/com/BarfaChat/Barfa/Controllers/UserController.java
@@ -1,23 +1,27 @@
 package com.BarfaChat.Barfa.Controllers;
+
 import com.BarfaChat.Barfa.Models.User;
 import com.BarfaChat.Barfa.Repositories.UserRepository;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 @RestController
 @RequestMapping(path = "api/v1/User")
 public class UserController {
-    @PostMapping
-    public void getUser(@RequestBody User user){
-        System.out.println(user.getEmail()+"|"+user.getFirstName()+"|"+user.getLastName()+"|"+user.getUsername());
-    }
+    private final UserRepository userRepository;
 
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
-    private final UserRepository userRepository;
-    public List<User> getUser(){
+
+    @PostMapping
+    public void getUser(@RequestBody User user) {
+        System.out.println(user.getEmail() + "|" + user.getFirstName() + "|" + user.getLastName() + "|" + user.getUsername());
+    }
+
+    @GetMapping
+    public List<User> getUser() {
         return userRepository.findAll();
     }
 }
