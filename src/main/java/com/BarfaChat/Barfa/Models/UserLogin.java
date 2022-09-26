804f7f79c68835cd5d21ca385e9532f27a03d4c1
Razvan <rotariurazvan2@gmail.com>
2022-09-26 11:45:57 +0100

    UserLogin Class added

---
+++ src/main/java/com/BarfaChat/Barfa/Models/UserLogin.java
@@ -0,0 +1,27 @@
+package com.BarfaChat.Barfa.Models;
+
+public class UserLogin {
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserLogin(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    private String userName;
+    private String password;
+}
