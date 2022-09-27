626bf56fbf8ef29e524aba1a4e744f9dcbd25a4f
Razvan <rotariurazvan2@gmail.com>
2022-09-27 17:34:41 +0100

    UserLogin Class added UserService added

--- src/main/java/com/BarfaChat/Barfa/Services/UserService.java
+++ src/main/java/com/BarfaChat/Barfa/Services/UserService.java
@@ -6,26 +6,30 @@ import com.BarfaChat.Barfa.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-//@Service
-//public class UserService implements UserService {
-//    UserRepository userRepository;
-//
-//    @Autowired
-//    public UserService(UserRepository userRepository) {
-//        super();
-//        this.userRepository = userRepository;
-//    }
-//
-//    @Override
-//    public UserLogin loadUserByUsername(String userName) throws UsernameNotFoundException {
-//        if (userName == null || userName.isEmpty()) {
-//            throw new UsernameNotFoundException("Username is Empty");
-//        }
-//
-//        User user = userRepository.findBy(userName);
-//        if (user != null) {
-//            return user.toCurrentUserDetails();
-//        }
-//        throw new UsernameNotFoundException(userName + "is not found !!!");
-//    }
-//}
+import java.util.List;
+
+@Service
+public class UserService {
+    UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        super();
+        this.userRepository = userRepository;
+    }
+    public UserLogin loadUserByUsername(String userName) {
+        if (userName == null || userName.isEmpty()) {
+            throw new IllegalArgumentException("Username cannot be empty");
+        }
+
+        User user = userRepository.findBy(userName);
+        if (user != null) {
+            return user.toCurrentUserDetails();
+        }
+        throw new IllegalArgumentException("Username is not Found!");
+    }
+
+
+    public List<User> findAll() {
+    }
+}
