15d3f4be8adb7cd4bd6a3dba10b7c991b2e7e4c1
Razvan <rotariurazvan2@gmail.com>
2022-09-30 16:58:08 +0100

    LoginController, LoginService, Added a Role

--- src/main/java/com/BarfaChat/Barfa/Controllers/LoginController.java
+++ src/main/java/com/BarfaChat/Barfa/Controllers/LoginController.java
@@ -1,55 +1,55 @@
-//package com.springboot.blog.controller;
-//import com.BarfaChat.Barfa.Repositories.UserRepository;
-//import com.springboot.blog.entity.Role;
-//import com.springboot.blog.entity.User;
-//import com.springboot.blog.payload.LoginDto;
-//import com.springboot.blog.payload.SignUpDto;
-//import com.springboot.blog.repository.RoleRepository;
-//import com.springboot.blog.repository.UserRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//package com.springboot.blog.payload;
-//import lombok.Data;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.Collections;
-//
+package com.springboot.blog.controller;
+import com.BarfaChat.Barfa.Repositories.UserRepository;
+import com.springboot.blog.entity.Role;
+import com.springboot.blog.entity.User;
+import com.springboot.blog.payload.LoginDto;
+import com.springboot.blog.payload.SignUpDto;
+import com.springboot.blog.repository.RoleRepository;
+import com.springboot.blog.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+package com.springboot.blog.payload;
+import lombok.Data;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
 
 
-//@Data
-//public class LoginDto {
-//    private String usernameOrEmail;
-//    private String password;
-//}
-//
-//@RestController
-//@RequestMapping("/api/auth")
-//public class LoginController {
-//
-//    @Autowired
-//    private AuthenticationManager authenticationManager;
-//
-//    @Autowired
-//    private UserRepository userRepository;
-//
-//    @Autowired
-//    private RoleRepoitory roleRepository;
-//
-//    @PostMapping("/login")
-//    public ResponseEntity<String> authenticateUser(@RequestBody LoginDto loginDto){
-//        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-//                loginDto.getUsernameOrEmail(), loginDto.getPassword()));
-//
-//        SecurityContextHolder.getContext().setAuthentication(authentication);
-//        return new ResponseEntity<>("User signed-in successfully!.", HttpStatus.OK);
-//    }
-//}
+
+@Data
+public class LoginDto {
+    private String usernameOrEmail;
+    private String password;
+}
+
+@RestController
+@RequestMapping("/api/auth")
+public class LoginController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> authenticateUser(@RequestBody LoginDto loginDto){
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                loginDto.getUsernameOrEmail(), loginDto.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return new ResponseEntity<>("User signed-in successfully!.", HttpStatus.OK);
+    }
+}
--- src/main/java/com/BarfaChat/Barfa/Models/User.java
+++ src/main/java/com/BarfaChat/Barfa/Models/User.java
@@ -4,11 +4,12 @@ import com.fasterxml.jackson.annotation.JsonAnyGetter;
 
 import javax.persistence.*;
 
-//package com.springboot.blog.entity;
-//import lombok.Getter;
-//import lombok.Setter;
+package com.springboot.blog.entity;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Table(name="users")
@@ -70,11 +71,11 @@ public class User {
     private String username;
     private String password;
 
-//    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-//    @JoinTable(name = "user_roles",
-//            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
-//            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
-//    private Set<Role> roles;
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private Set <Role> roles;
 
     public User(String firstName, String lastName, String email, String username) {
         this.firstName = firstName;
@@ -93,15 +94,15 @@ public class User {
                 ", password='" + password + '\'' +
                 '}';
     }
-//    @Entity
-//    @Table(name = "roles")
-//    public class Role {
-//
-//        @Id
-//        @GeneratedValue(strategy = GenerationType.IDENTITY)
-//        private long id;
-//
-//        @Column(length = 60)
-//        private String name;
-//    }
+    @Entity
+    @Table(name = "roles")
+    public class Role {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private long id;
+
+        @Column(length = 60)
+        private String name;
+    }
 }
