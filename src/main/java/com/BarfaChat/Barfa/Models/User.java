07ea175d5f75b838ca0561bf159d36c74066c21d
Razvan <rotariurazvan2@gmail.com>
2022-09-30 16:58:08 +0100

    LoginController, LoginService, Added a Role

--- src/main/java/com/BarfaChat/Barfa/Models/User.java
+++ src/main/java/com/BarfaChat/Barfa/Models/User.java
@@ -1,6 +1,15 @@
 package com.BarfaChat.Barfa.Models;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+
 import javax.persistence.*;
+
+//package com.springboot.blog.entity;
+//import lombok.Getter;
+//import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Table(name="users")
@@ -62,11 +71,11 @@ public class User {
     private String username;
     private String password;
 
-//    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-//    @JoinTable(name = "user_roles",
-//            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
-//            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
-//    private Set<Role> roles;
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private Set <Role> roles;
 
     public User(String firstName, String lastName, String email, String username, String password) {
         this.firstName = firstName;
@@ -86,15 +95,15 @@ public class User {
                 ", password='" + password + '\'' +
                 '}';
     }
-//    @Entity
-//    @Table(name = "roles")
-//    public class Role {
-//
-//        @Id
-//        @GeneratedValue(strategy = GenerationType.IDENTITY)
-//        private long id;
-//
-//        @Column(length = 60)
-//        private String name;
-//    }
+    @Entity
+    @Table(name = "roles")
+    public class Role {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private long id;
+
+        @Column(length = 60)
+        private String name;
+    }
 }
